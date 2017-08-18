8f51175f984ea35d030a2fd5bb011bacf94e407a
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2017-08-18 10:23:54 +0200

    Add fpromise example

--- promises/f-promise-catch.js
+++ promises/f-promise-catch.js
@@ -1,7 +1,7 @@
 let wait = require('f-promise').wait;
 let run = require('f-promise').run;
 
-// cette fonction jette une erreur après deux secodnes
+// cette fonction jete une erreur après deux secodnes
 function resolveAfterNSeconds(x) {
     return new Promise(resolve => {
         setTimeout(() => {
@@ -20,8 +20,34 @@ run(() => {
     console.log("value2: " + value2);
 
 }).catch(function () {
-    throw new Error();
+    throw new Error("resolveAfterNSeconds");
+});
+
+// cette fonction retourne une promesse rejetée après deux secodnes
+function rejectAfterNSeconds(x) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            // /!\/!\/!\/!\ si une erreur n'est pas passée, fpromise ne jette pas
+            // d'erreurs à l'instruction wait()
+
+            reject(new Error("Error from resolveAfterNSeconds!"));
+
+        }, 800);
+    });
+}
+
+run(() => {
+
+    let value = wait(rejectAfterNSeconds(5));
+    console.log("value: " + value);
+
+    let value2 = wait(rejectAfterNSeconds(10));
+    console.log("value2: " + value2);
+
+}).catch(function () {
+    throw new Error("rejectAfterNSeconds");
 });
 
 
-    
+
