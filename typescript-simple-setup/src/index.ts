556fe0c5677e4aa0b0bfd3f9f9cd9b5cfe05107f
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2017-10-22 11:18:40 +0200

    Add ts simple setup example

---
+++ typescript-simple-setup/src/index.ts
@@ -0,0 +1,5 @@
+export class HelloWorld {
+    public hello() {
+        console.log("Hello world !");
+    }
+}
---
+++ typescript-simple-setup/src/tests/indexTest.ts
@@ -0,0 +1,14 @@
+import * as chai from 'chai';
+//import * as sinon from 'sinon';
+
+require('source-map-support').install();
+
+const assert = chai.assert;
+
+describe('First test', () => {
+
+    it('Say hello !', () => {
+        assert.isTrue(false);
+    })
+
+});
