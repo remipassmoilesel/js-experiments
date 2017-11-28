5fa4d5aec708372694b19cbc96503d013b554761
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2017-11-28 13:30:11 +0100

    Add create example

---
+++ tingodb/createCollection.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var Db = require('tingodb')().Db,
+    assert = require('assert');
+
+var db = new Db('./db', {});
+
+// Fetch a collection to insert document into
+var collection = db.createCollection('collection1');
+
+// Insert a single document
+collection.insert(
+    [{hello:'world_safe1'}, {hello:'world_safe2'}], {w:1}, function(err, result) {
+    assert.equal(null, err);
+
+    // Fetch the document
+    collection.findOne({hello:'world_safe2'}, function(err, item) {
+        assert.equal(null, err);
+        assert.equal('world_safe2', item.hello);
+    });
+});
